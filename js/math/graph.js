9dfbffd52c52b57a78ab7e46fe26e35f83e20257
yohan <yohanbinan22@gmail.com>
2024-03-08 00:12:49 +0100

    I've done the new interface, move on the road

--- js/math/graph.js
+++ js/math/graph.js
@@ -4,6 +4,16 @@ class Graph{
         this.segments = segments;
     }
 
+    static load(info){
+        const points = info.points.map((i) => new Point(i.x, i.y));
+        const segments = info.segments.map((i) => new Segment(
+            points.find((p) => p.equals(i.p1)),
+            points.find((p) => p.equals(i.p2))
+        ));
+        
+        return new Graph(points, segments);
+    }
+
     tryAddPoint(point){
         if(!this.containsPoint(point)){
             this.addPoint(point);
