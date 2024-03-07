e169b73eccbb7f6fe5f275ffd4746a1dbd4404c4
yohan <yohanbinan22@gmail.com>
2024-03-07 23:27:13 +0100

    I can zoom and drag the canvas to change the focus

--- js/viewport.js
+++ js/viewport.js
@@ -4,6 +4,12 @@ class Viewport{
         this.ctx = canvas.getContext("2d");
         this.zoom = 1;
         this.offset = new Point(0, 0);
+        this.drag = {
+            start : new Point(0, 0),
+            end : new Point(0, 0),
+            offset : new Point(0, 0),
+            active : false
+        };
 
         this.#addEventListeners();
     }
@@ -17,6 +23,9 @@ class Viewport{
 
     #addEventListeners(){
         this.canvas.addEventListener("mousewheel", this.#handlerMouseWheel.bind(this));
+        this.canvas.addEventListener("mousedown", this.#handlerMouseDown.bind(this));
+        this.canvas.addEventListener("mousemove", this.#handlerMouseMove.bind(this));
+        this.canvas.addEventListener("mouseup", this.#handlerMouseUp.bind(this));
     }
 
     #handlerMouseWheel(evt){
@@ -24,5 +33,35 @@ class Viewport{
         const step = 0.1;
         this.zoom += dir*step;
         this.zoom = Math.max(1, Math.min(5, this.zoom));
+    }
+
+    #handlerMouseDown(evt){
+        if (evt.button == 1){ // middle button
+            this.drag.start = this.getMouse(evt);
+            this.drag.active = true;
+        }
+    }
+
+    #handlerMouseMove(evt){
+        if (this.drag.active){
+            this.drag.end = this.getMouse(evt);
+            this.drag.offset = subtract(this.drag.end, this.drag.start);
+        }
+    }
+
+    #handlerMouseUp(evt){
+        if (this.drag.active){
+            this.offset = add(this.offset, this.drag.offset);
+            this.drag = {
+                start : new Point(0, 0),
+                end : new Point(0, 0),
+                offset : new Point(0, 0),
+                active : false
+            };
+        }
+    }
+
+    getOffset(){
+        return add(this.offset, this.drag.offset);
     }
 }
