ad45dc9f673777a714b0992d10b59b7c25d45bd0
yohan <yohanbinan22@gmail.com>
2024-03-07 15:37:21 +0100

    I've done the segment features (adding, removing)

--- js/graphEditor.js
+++ js/graphEditor.js
@@ -3,6 +3,7 @@ class GraphEditor{
         this.canvas = canvas;
         this.graph = graph;
         this.ctx = this.canvas.getContext("2d");
+        this.mouse = null;
         this.selected = null;
         this.hovered  = null;
         this.dragging = false;
@@ -16,34 +17,43 @@ class GraphEditor{
             if (evt.button == 2){ //right click
                 if (this.hovered){
                     this.#removePoint(this.hovered);
+                } else {
+                    this.selected = null
                 }
             }
             if (evt.button == 0){// left click
-                const mouse = new Point(evt.offsetX, evt.offsetY);
                 if (this.hovered){
+                    this.#selectPoint(this.hovered);
                     this.selected = this.hovered;
                     this.dragging = true;
                     return;
                 }
-                this.graph.addPoint(mouse);
-                this.selected = mouse;
-                this.hovered = mouse;
+                this.graph.addPoint(this.mouse);
+                this.#selectPoint(this.mouse);
+                this.hovered = this.mouse;
             }
         } );
 
         // Event 2
         this.canvas.addEventListener("mousemove", (evt) => {
-            const mouse = new Point(evt.offsetX, evt.offsetY);
-            this.hovered = getNearestPoint(mouse, this.graph.points, 10);
+            this.mouse = new Point(evt.offsetX, evt.offsetY);
+            this.hovered = getNearestPoint(this.mouse, this.graph.points, 10);
             if (this.dragging == true){
-                this.selected.x = mouse.x;
-                this.selected.y = mouse.y;
+                this.selected.x = this.mouse.x;
+                this.selected.y = this.mouse.y;
             }
         } );
         // Event 3 
         this.canvas.addEventListener("contextmenu", (evt) => evt.preventDefault());
         // Event 4 
         this.canvas.addEventListener("mouseup", () => this.dragging = false);
+    }
+
+    #selectPoint(point){
+        if (this.selected){
+            this.graph.tryAddSegment(new Segment(this.selected, point));
+        }
+        this.selected = point;
     }
 
     #removePoint(point){
@@ -60,6 +70,8 @@ class GraphEditor{
             this.hovered.draw(this.ctx, { fill: true})
         }
         if (this.selected){
+            const intent = this.hovered ? this.hovered : this.mouse;
+            new Segment(this.selected, intent).draw(ctx, {dash: [3,3]});
             this.selected.draw(this.ctx, { outline: true})
         }
     }
