55160c5d9d30712fd8c02a5e96d9807ed238fa47
yohan <yohanbinan22@gmail.com>
2024-03-08 09:30:58 +0100

    I've done the road implementation

--- js/primitives/envelope.js
+++ js/primitives/envelope.js
@@ -25,8 +25,8 @@ class Envelope{
         return new Polygon(points);
     }
 
-    draw(ctx){
-        this.poly.draw(ctx);
+    draw(ctx, options){
+        this.poly.draw(ctx, options);
     }
 
 }
