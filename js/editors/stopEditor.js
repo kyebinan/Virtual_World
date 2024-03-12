360f63f3cd74f5cad469cf114e588af2733eea5f
yohan <yohanbinan22@gmail.com>
2024-03-12 16:14:19 +0100

    Refactoring the stopEditor and the crossing editor

--- js/editors/stopEditor.js
+++ js/editors/stopEditor.js
@@ -1,92 +1,14 @@
-class StopEditor{
+class StopEditor extends MarkingEditor{
     constructor(viewport, world){
-        this.viewport = viewport;
-        this.world = world;
-
-        this.canvas = viewport.canvas;
-        this.ctx = this.canvas.getContext("2d");
-
-        this.mouse = null;
-        this.intent = null;
-
-        this.markings = world.markings;
+        super(viewport, world, world.laneGuides);
     }
 
-    enable(){
-        this.#addEventListeners();
-    }
-
-    disable(){
-        this.#removeEventListeners();
-    }
-
-    #addEventListeners(){
-        this.boundMouseDown = this.#handleMouseDown.bind(this);
-        this.boundMouseMove = this.#handleMouseMove.bind(this);
-        this.boundContextMenu = (evt) => evt.preventDefault();
-        // Event 1 
-        this.canvas.addEventListener("mousedown", this.boundMouseDown);
-        // Event 2
-        this.canvas.addEventListener("mousemove", this.boundMouseMove);
-        // Event 3 
-        this.canvas.addEventListener("contextmenu", this.boundContextMenu);
-        
-    }
-
-    #removeEventListeners(){
-        // Event 1 
-        this.canvas.removeEventListener("mousedown", this.boundMouseDown);
-        // Event 2
-        this.canvas.removeEventListener("mousemove", this.boundMouseMove);
-        // Event 3
-        this.canvas.removeEventListener("contextmenu", this.boundContextMenu);
-        
-    }
-
-    #handleMouseMove(evt){
-        this.mouse = this.viewport.getMouse(evt, true);
-        const seg = getNearestSegment(
-            this.mouse, 
-            this.world.laneGuides, 
-            10 * this.viewport.zoom
-        );
-        if (seg){
-            const proj = seg.projectPoint(this.mouse);
-            if (proj.offset >= 0 && proj.offset <= 1){
-                this.intent = new Stop(
-                    proj.point,
-                    seg.directionVector(),
-                    this.world.roadWidth / 2,
-                    this.world.roadWidth / 2 
-                );
-            } else {
-                this.intent = null;
-            }
-        } else {
-            this.intent = null;
-        }
-    }
-
-    #handleMouseDown(evt){
-        if (evt.button == 0){
-            if (this.intent){
-                this.markings.push(this.intent)
-            }
-        }
-        if (evt.button == 2){
-            for (let i = 0; i < this.markings.length; i++){
-                const poly = this.markings[i].poly;
-                if (poly.containsPoint(this.mouse)){
-                    this.markings.splice(i, 1);
-                    return;
-                }
-            }
-        }
-    }
-
-    display(){
-        if (this.intent){
-            this.intent.draw(this.ctx);
-        }
+    createMarking(center, directionVector){
+        return new Stop(
+            center,
+            directionVector,
+            world.roadWidth / 2,
+            world.roadWidth / 2
+        )
     }
 }
