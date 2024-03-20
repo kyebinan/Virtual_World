3c8b1dcd87524439df149ab77b1bb3d75bbb3cd0
yohan <yohanbinan22@gmail.com>
2024-03-20 19:37:56 +0100

    I've finish the car physics

--- SELF_DRIVING_CAR/controls.js
+++ SELF_DRIVING_CAR/controls.js
@@ -5,51 +5,41 @@ class Controls{
         this.right=false;
         this.reverse=false;
 
-        this.#addKeyBoardListeners();
-
+        this.#addKeyboardListeners();
     }
 
-    #addKeyBoardListeners(){
-        document.onkeydown=(event) => {
+    #addKeyboardListeners(){
+        document.onkeydown=(event)=>{
             switch(event.key){
                 case "ArrowLeft":
                     this.left=true;
                     break;
-
                 case "ArrowRight":
                     this.right=true;
                     break;
-                
                 case "ArrowUp":
                     this.forward=true;
                     break;
-
                 case "ArrowDown":
                     this.reverse=true;
                     break;
             }
-            console.table(this);
         }
-
-        document.onkeyup=(event) => {
+        document.onkeyup=(event)=>{
             switch(event.key){
                 case "ArrowLeft":
                     this.left=false;
                     break;
-
                 case "ArrowRight":
                     this.right=false;
                     break;
-                
                 case "ArrowUp":
                     this.forward=false;
                     break;
-
                 case "ArrowDown":
                     this.reverse=false;
                     break;
             }
-            console.table(this);
         }
     }
 }
