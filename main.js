928de28ae8bda66219c028f55e5b2e3dbae2a210
yohan <yohanbinan22@gmail.com>
2024-03-23 00:38:40 +0100

    Adding miniMap

--- main.js
+++ main.js
@@ -2,19 +2,25 @@ const carCanvas=document.getElementById("carCanvas");
 carCanvas.width=window.innerWidth - 330;
 const networkCanvas=document.getElementById("networkCanvas");
 networkCanvas.width=300;
+const miniMapCanvas=document.getElementById("miniMapCanvas");
+miniMapCanvas.width=300;
+miniMapCanvas.height=300;
 
 carCanvas.height=window.innerHeight;
-networkCanvas.height=window.innerHeight;
+networkCanvas.height=window.innerHeight-300;
 
 const carCtx = carCanvas.getContext("2d");
 const networkCtx = networkCanvas.getContext("2d");
+
 
 const worldString = localStorage.getItem("world");
 const worldInfo = worldString ? JSON.parse(worldString) : null;
 const world = worldInfo
    ? World.load(worldInfo)
    : new World(new Graph());
+
 const viewport = new Viewport(carCanvas, world.zoom, world.offset);
+const miniMap = new MiniMap(miniMapCanvas, world.graph, 300);
 
 const N=100;
 const cars=generateCars(N);
@@ -81,6 +87,7 @@ function animate(time){
     viewport.reset();
     const viewPoint = scale(viewport.getOffset(), -1);
     world.draw(carCtx, viewPoint, false);
+    miniMap.update(viewPoint);
 
     for(let i=0;i<traffic.length;i++){
         traffic[i].draw(carCtx);
